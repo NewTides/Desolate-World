5e0ecbf6bac9db4d3b5456303b763adc632ab1cd
NewTides <jlevy.75583@farnborough.ac.uk>
2021-04-19 13:07:25 +0100

    Final pre-test commit

---
+++ Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    private RaycastHit vision;
+    public float rayLength;
+    private bool isGrabbed;
+    private Rigidbody grabbedObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayLength = 6.0f;
+        isGrabbed = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Dubug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, Color.red, 0.5f);
+        
+        
+    }
+}
